aee3a1af2a3f3ded323f53cdddb6c5f6bdb276f9
vetaly <vitalik>
2017-11-06 17:39:50 +0300

    Add ReRun, randomize array items,  see color of the square

--- src/App.js
+++ src/App.js
@@ -2,26 +2,36 @@
 import React from 'react';
 import styled from 'styled-components';
 import { compose, withState, withHandlers } from 'recompose';
-import { flatten } from 'lodash';
+import { flatten, findIndex } from 'lodash';
 
 import GameBoard from './GameBoard';
 
 import { GameBoardSize, colorArray, defaultColor } from './GameConstants';
 
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 const Container = styled.div`
 `;
 
 const Score = styled.div`
-color: black;
-height: 100px;`;
+  color: black;
+  height: 100px;`;
 
 const Game = ({
   round,
   squareArray,
-  toggleSquareColor
+  toggleSquareColor,
+  reRun
    }) => (
     <Container>
-      <Score>Round:{round}</Score>
+      <Score>Round:{round}<button onClick={reRun}>Re-Run</button></Score>
       <GameBoard
         squareArray={squareArray}
         defaultColor={defaultColor}
@@ -50,19 +60,20 @@ function SquareArray(colorArrayArg) {
   })
 
   const concatSquareArray = flatten(squareArray);
-  return concatSquareArray;
+  const randomizeArray = shuffleArray(concatSquareArray);
+  return randomizeArray;
 }
 
 function changeSquareArray(squareArray, setSquareArray, id) {
-  const elementIndex = squareArray.findIndex((element) => { return id === element.id; });
+  const elementIndex = findIndex(squareArray, (element) => { return id === element.id; });
+debugger; 
   const changedSquareObject = Object.assign(
     {},
     squareArray[elementIndex],
     { clicked: !squareArray[elementIndex].clicked }
   );
   squareArray[elementIndex] = changedSquareObject;
-
-  return setSquareArray;
+  setSquareArray(squareArray);
 }
 
 const enhance = compose(
@@ -83,18 +94,27 @@ const enhance = compose(
           props.setRound(newRound); 
           props.setSelectedElementId(0);
           props.setSelectedElementColor('');
-
           changeSquareArray(props.squareArray, props.setSquareArray, id);
         }
         else {
           const newRound =props.round + 1; 
-          props.setRound(newRound); 
-          props.setSelectedElementColor('');
-          changeSquareArray(props.squareArray, props.setSquareArray, props.selectedElementId);
-          props.setSelectedElementId(0);
+          changeSquareArray(props.squareArray, props.setSquareArray, id);
+          setTimeout(() => {
+            props.setRound(newRound); 
+            props.setSelectedElementColor('');
+            changeSquareArray(props.squareArray, props.setSquareArray, id);
+            debugger; 
+            changeSquareArray(props.squareArray, props.setSquareArray, props.selectedElementId);
+            props.setSelectedElementId(0);
+          }, 1000);
         }
       }
     },
+    reRun: props => () =>{
+
+      props.setRound(1); 
+      props.setSquareArray(SquareArray(colorArray)); 
+    }
   }),
 );
 
